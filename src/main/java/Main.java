767f818b062d07752b51933a86bff98f558ac408
Austin Ray <austinray1995@gmail.com>
2017-04-02 15:54:25 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  /**
+   * Entry point into the program.
+   * Program will be launched through this method.
+   * @param args    Command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
